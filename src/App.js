5f64f553772cf269c76f331e6d3ee83e290777a6
sovazavr <vadimsopov00@mail.ru>
2022-01-23 11:58:13 +0700

    pre final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { randomColor } from 'randomcolor'
+import Draggable from 'react-draggable';
 import './App.css';
 
+
 function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items]);
+
+
+  const newItem = () => {
+    if (item.trim() !== '') {
+      const newTask = {
+        id: uuidv4(),
+        item,
+        timeAdd:  new Date().getFullYear()+" "+new Date().getDate()+" "+new Date().toLocaleDateString('default', { month: 'short', weekday: 'short',}) +" "+ new Date().getHours().toLocaleString()+":"+ new Date().getMinutes().toLocaleString(),
+        color: randomColor({
+          luminosity: 'light'
+        }),
+        defaultPosition: {
+          x: 500,
+          y: -500
+        }
+      }
+      setItems((items) => [...items, newTask])
+      setItem('')
+    } else {
+      alert('Заполни инпут мразь')
+      setItem('')
+    }
+  }
+
+  const deleteTask = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+  const updatePosition=(data,index)=>{
+    let newArrayItems=[...items]
+    newArrayItems[index].defaultPosition={x:data.x, y:data.y}
+    setItems(newArrayItems)
+  }
+
+  const pressEnter=(e)=>{
+    const keyCode= e.keyCode || e.which
+    if(keyCode===13) {
+      newItem()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='wrapper'>
+        <input value={item} className='inputtask' type='text'
+          placeholder='Enter'
+          onChange={(e) => setItem(e.target.value)}
+          onKeyPress={(e)=>pressEnter(e)}></input>
+        <button className='enter' onClick={newItem}>Enter</button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable key={index} defaultPosition={item.defaultPosition} onStop={(_, data)=>{updatePosition(data, index)}}>
+            <div className='todo' style={{ backgroundColor: item.color }}>
+              {`${item.item}`}
+              <div className='timeAdd'>добавлено: {`${item.timeAdd}`}</div>
+              <button className='delete' onClick={() => deleteTask(item.id)}>x</button>
+            </div>
+
+          </Draggable>
+        )
+      })} 
+      
     </div>
   );
 }
